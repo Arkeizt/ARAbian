bd15119a3c213d73b89398b3738eb2fa67651525
ketsupL <neilmorales557@gmail.com>
2025-04-10 14:31:03 +0800

    Updated app-logo-icom.tsx

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,8 +1,12 @@
 import { SVGAttributes } from 'react';
 
-export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
+interface AppLogoIconProps extends SVGAttributes<SVGElement> {
+    className?: string; // Allow custom className to be passed
+}
+
+export default function AppLogoIcon({ className = '' }: AppLogoIconProps) {
     return (
-        <div className="bg-white rounded-md">
+        <div className={`bg-white rounded-md ${className}`}>
             <img src="/asc.svg" alt="App Logo" className="w-20" />
         </div>
     );
