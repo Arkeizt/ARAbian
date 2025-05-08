e297059518ebbdb7514bf4259ef802541e24f89f
Arkeizt <fionnahkeiz@gmail.com>
2025-05-09 05:11:23 +0800

    Add RequestProject page

---
+++ resources/js/pages/client/RequestProject.tsx
@@ -0,0 +1,10 @@
+export default function RequestProject() {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold">Request a Project</h1>
+            <p className="mt-2 text-gray-700 dark:text-gray-300">
+                fill out forms for requesting a project
+            </p>
+        </div>
+    );
+}
